c9ee6b2fa2fd7084968812436d5c800ecf6478b8
Dylan Warrell <d.warrell@outlook.com>
2023-02-08 22:05:07 +0000

    Google maps setup

--- src/app/pages/maps/maps.component.ts
+++ src/app/pages/maps/maps.component.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { GoogleMap, GoogleMapsModule } from '@angular/google-maps';
 import { Observable, map, catchError, of } from 'rxjs';
-declare const google: any;
 
 @Component({
   selector: 'app-maps',
@@ -10,21 +10,59 @@ declare const google: any;
 })
 export class MapsComponent implements OnInit {
 
-  apiLoaded: Observable<boolean>;
-
+  // ---- MAP INIT ----
   constructor(httpClient: HttpClient) {
-    this.apiLoaded = httpClient.jsonp('https://maps.googleapis.com/maps/api/js?key=AIzaSyASHU1WvCipdeZGJoIeI-TQkLKoPur3PDE', 'callback')
+    httpClient.jsonp('https://maps.googleapis.com/maps/api/js?key=AIzaSyASHU1WvCipdeZGJoIeI-TQkLKoPur3PDE', 'callback')
         .pipe(
           map(() => true),
           catchError(() => of(false)),
         );
   }
 
+  // ---- BOWSER MARKER ICONS ----
+  center: google.maps.LatLngLiteral = {lat: 51.8994, lng: -2.0783};
+  userLocationIcon = {
+    url: "http://maps.google.com/mapfiles/kml/shapes/homegardenbusiness.png",
+    scaledSize: new google.maps.Size(35, 35),
+    origin: new google.maps.Point(0,0),
+    anchor: new google.maps.Point(17.5, 17.5)
+  };
+  bowserIcon = {
+    url: "http://maps.google.com/mapfiles/kml/shapes/water.png",
+    scaledSize: new google.maps.Size(35, 35),
+    origin: new google.maps.Point(0,0),
+    anchor: new google.maps.Point(17.5, 17.5)
+  };
+  bowserSpannerIcon = {
+    url: "http://maps.google.com/mapfiles/kml/shapes/mechanic.png",
+    scaledSize: new google.maps.Size(35, 35),
+    origin: new google.maps.Point(0,0),
+    anchor: new google.maps.Point(17.5, 17.5)
+  };
+  bowserOfflineIcon = {
+    url: "http://maps.google.com/mapfiles/kml/shapes/caution.png",
+    scaledSize: new google.maps.Size(35, 35),
+    origin: new google.maps.Point(0,0),
+    anchor: new google.maps.Point(17.5, 17.5)
+  };
+
+  // ---- ICON OPTIONS ----
+  userMarkerOptions: google.maps.MarkerOptions = {draggable: false, icon: this.userLocationIcon};
+  bowserMarkerOptions: google.maps.MarkerOptions = {draggable: false, icon: this.bowserIcon};
+  bowserSpannerMarkerOptions: google.maps.MarkerOptions = {draggable: false, icon: this.bowserSpannerIcon};
+  bowserOfflineMarkerOptions: google.maps.MarkerOptions = {draggable: false, icon: this.bowserOfflineIcon};
+
+  // ---- INIT LOCATION ARRAY ----
+  bowsersPositions: google.maps.LatLngLiteral[] = [];
+  bowserSpannerPositions: google.maps.LatLngLiteral[] = [];
+  bowserOfflinePositions: google.maps.LatLngLiteral[] = [];
+  userLocation: google.maps.LatLngLiteral[] = [];
+
+  // ---- MAP OPTIONS ----
   options: google.maps.MapOptions = {
-    zoom: 13,
-    scrollwheel: false,
-    center: {lat: 51.89948430584023, lng: -2.0782783087952157},
+    zoom: 14,
     mapTypeId: google.maps.MapTypeId.ROADMAP,
+    streetViewControl: false,
     styles: [
       {"featureType":"administrative","elementType":"labels.text.fill","stylers":[{"color":"#444444"}]},
       {"featureType":"landscape","elementType":"all","stylers":[{"color":"#f2f2f2"}]},
@@ -34,42 +72,20 @@ export class MapsComponent implements OnInit {
       {"featureType":"road.arterial","elementType":"labels.icon","stylers":[{"visibility":"off"}]},
       {"featureType":"transit","elementType":"all","stylers":[{"visibility":"off"}]},
       {"featureType":"water","elementType":"all","stylers":[{"color":'#5e72e4'},{"visibility":"on"}]}]
-
-
-     /* [
-        {"elementType":"labels","stylers":[{"visibility":"off"}]},
-        {"featureType":"administrative","elementType":"labels.text.fill","stylers":[{"color":"#444444"}]},
-        {"featureType":"administrative.land_parcel","stylers":[{"visibility":"off"}]},{"featureType":"administrative.neighborhood","stylers":[{"visibility":"simplified"}]},
-        {"featureType":"poi.government","stylers":[{"visibility":"simplified"}]},{"featureType":"poi.medical","stylers":[{"visibility":"simplified"}]},
-        {"featureType":"poi.park","elementType":"labels","stylers":[{"visibility":"simplified"}]},{"featureType":"road","stylers":[{"saturation":-100},{"lightness":30}]},
-        {"featureType":"transit.station.airport","elementType":"labels","stylers":[{"visibility":"simplified"}]},
-        {"featureType":"transit.station.rail","stylers":[{"visibility":"simplified"}]},
-        {"featureType":"water","stylers":[{"color":"#5e72e4"},{"visibility":"on"}]}]*/
-
   };
+
+  // ------- END OF MAP SETUP -------
   
   ngOnInit() {
-/*
-    map = new google.maps.Map(map, mapOptions);
-
-    var marker = new google.maps.Marker({
-        position: myLatlng,
-        map: map,
-        animation: google.maps.Animation.DROP,
-        title: 'Hello World!'
-    });
-
-    var contentString = '<div class="info-window-content"><h2>Argon Dashboard</h2>' +
-        '<p>A beautiful Dashboard for Bootstrap 4. It is Free and Open Source.</p></div>';
-
-    var infowindow = new google.maps.InfoWindow({
-        content: contentString
-    });
-
-    google.maps.event.addListener(marker, 'click', function() {
-        infowindow.open(map, marker);
-    });
-*/
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(position => {
+        this.userLocation.push({lat: position.coords.latitude, lng: position.coords.longitude});
+      });    
+    }
   }
 
+  addBowser(event) {
+    this.bowsersPositions.push(event.latLng.toJSON());
+  }
+  
 }
