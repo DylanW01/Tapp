ee3e590729db347f1bad0eaa4267a225087a5a76
Dylan Warrell <124311289+DylanW01@users.noreply.github.com>
2023-02-18 23:40:33 +0000

    Enable routing hash

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -51,7 +51,7 @@ const routes: Routes = [
   imports: [
     CommonModule,
     BrowserModule,
-    RouterModule.forRoot(routes),
+    RouterModule.forRoot(routes,  { useHash: true }),
   ],
   exports: [],
 })
