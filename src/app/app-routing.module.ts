72bbec43db614d8424a3bc41d2374c76c99e4d05
Dylan Warrell <124311289+DylanW01@users.noreply.github.com>
2023-02-19 00:02:37 +0000

    This better work

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -42,7 +42,7 @@ const routes: Routes = [
     path: "**",
     redirectTo: "dashboard",
   },
-  { path: 'login/callback',
+  { path: '#/login/callback',
     component: OktaCallbackComponent
   },
 ];
