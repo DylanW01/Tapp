3a3186912b112182c6df06499ddc9fae70a02fb4
Dylan Warrell <124311289+DylanW01@users.noreply.github.com>
2023-05-20 21:52:03 +0100

    port number

--- tapp-server/src/app.js
+++ tapp-server/src/app.js
@@ -44,7 +44,7 @@ const swaggerDefinition = {
   },
   servers: [
     {
-      url: "https://tapp-server.onrender.com:443",
+      url: "https://tapp-server.onrender.com:1000",
       description: "Development server"
     }
   ]
