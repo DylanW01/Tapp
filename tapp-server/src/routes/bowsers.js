14250feb2b155e3578ab3cdb7c4b6f2ff59dd1b8
Dylan Warrell <124311289+DylanW01@users.noreply.github.com>
2023-09-20 14:56:22 +0100

    Remove hardcoded SendGrid API key

--- tapp-server/src/routes/bowsers.js
+++ tapp-server/src/routes/bowsers.js
@@ -41,7 +41,7 @@ function createRouter(db) {
           res.status(500).json({status: 'error'});
         } else {
           const sgMail = require('@sendgrid/mail')
-          sgMail.setApiKey('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')
+          sgMail.setApiKey(process.env.SENDGRIDAPI)
           const msg = {
             to: 'd.warrell@outlook.com',
             from: { "email": "noreply@tapp.dylanwarrell.com", 
@@ -74,7 +74,7 @@ function createRouter(db) {
           res.status(500).json({status: 'error'});
         } else {
           const sgMail = require('@sendgrid/mail')
-          sgMail.setApiKey('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')
+          sgMail.setApiKey(process.env.SENDGRIDAPI)
           const msg = {
             to: ['d.warrell@outlook.com'],
             from: { "email": "noreply@tapp.dylanwarrell.com", 
@@ -109,7 +109,7 @@ function createRouter(db) {
           res.status(500).json({status: 'error'});
         } else {
           const sgMail = require('@sendgrid/mail')
-          sgMail.setApiKey('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')
+          sgMail.setApiKey(process.env.SENDGRIDAPI)
           const msg = {
             to: ['d.warrell@outlook.com'],
             from: { "email": "noreply@tapp.dylanwarrell.com", 
