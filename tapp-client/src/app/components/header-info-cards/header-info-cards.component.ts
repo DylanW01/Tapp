d998528cfabd57257bd9e1271f7e49b25a5372ff
Dylan Warrell <124311289+DylanW01@users.noreply.github.com>
2024-01-29 16:52:46 +0000

    Attempt to add query timeout

--- tapp-client/src/app/components/header-info-cards/header-info-cards.component.ts
+++ tapp-client/src/app/components/header-info-cards/header-info-cards.component.ts
@@ -24,9 +24,21 @@ export class HeaderInfoCardsComponent implements OnInit {
       keyboard: false, // 'false' prevents closing with the keyboard ESC key
       centered: true,
     };
-    this.server
-      .getStats()
+
+    // Set a timeout for 4 seconds
+    const timeoutPromise = new Promise((_, reject) => {
+      setTimeout(() => reject(new Error("Query timeout")), 4000);
+    });
+
+    // Set a variable to store the timeout ID
+    let timeoutId: NodeJS.Timeout;
+
+    // Make the server request and race it with the timeout
+    Promise.race([this.server.getStats(), timeoutPromise])
       .then((response: any) => {
+        // Clear the timeout if the request succeeds within 4 seconds
+        clearTimeout(timeoutId);
+
         console.log(response);
         this.totalBowsers = response.bowsersCount;
         this.activeBowsers = response.activeBowsersCount;
@@ -35,12 +47,27 @@ export class HeaderInfoCardsComponent implements OnInit {
         this.bowsersDown = response.bowserDownCount;
       })
       .catch((error: any) => {
-        const modalRef = this.modalService.open(ServerOfflineModalComponent, modalOptions);
-        modalRef.result.then(() => {
-          // Modal closed, call getStats
-          this.getStats();
-        });
+        // Check if the error is due to a timeout
+        if (error.message === "Query timeout") {
+          // If it's a timeout, open the modal
+          const modalRef = this.modalService.open(ServerOfflineModalComponent, modalOptions);
+          modalRef.result.then(() => {
+            // Modal closed, call getStats
+            this.getStats();
+          });
+        } else {
+          // Handle other errors here
+          console.error(error);
+        }
       });
+
+    // Set the timeout ID
+    timeoutId = setTimeout(() => {
+      // If the request takes longer than 4 seconds, reject the timeout promise
+      clearTimeout(timeoutId);
+      timeoutPromise.catch((error) => console.error(error));
+    }, 4000);
+
     this.interval = setInterval(() => {
       this.getStats();
     }, 60000);
